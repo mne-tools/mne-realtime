5fa55e663f89e10a359d2fba019130ff52e91af7
Eric Larson <larson.eric.d@gmail.com>
2021-02-08 16:34:13 -0500

    FIX: Reqs

--- tools/pip_dependencies.sh
+++ tools/pip_dependencies.sh
@@ -2,7 +2,5 @@
 
 python -m pip install --upgrade pip
 python -m pip install --upgrade setuptools wheel
-python -m pip install numpy scipy matplotlib
 python -m pip install -r requirements.txt
-python -m pip install https://github.com/mne-tools/mne-python/archive/c304072d1c67912da9b4ac62fe64e07ed41f2dd6.tar.gz
 python -m pip install -ve .
