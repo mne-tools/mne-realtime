54fc8e693f3ab272bd884fd3f28822aa96cadbeb
charles <charles@ae.studio>
2023-04-11 12:21:58 -0700

    Pre-allocate buffer for `pylsl`'s `.pull_chunk` for faster reads (#47)
    thanks @charlesincharge!

--- mne_realtime/tests/test_lsl_client.py
+++ mne_realtime/tests/test_lsl_client.py
@@ -9,8 +9,10 @@ import pytest
 from mne.utils import requires_pylsl
 from mne.io import read_raw_fif
 from mne.datasets import testing
+import numpy as np
 
 from mne_realtime import LSLClient, MockLSLStream
+from mne_realtime.lsl_client import _check_pylsl_installed
 
 base_dir = op.join(op.dirname(__file__), 'data')
 
@@ -23,18 +25,53 @@ raw_fname = op.join(base_dir, 'test_raw.fif')
 def test_lsl_client():
     """Test the LSLClient for connection and data retrieval."""
     raw = read_raw_fif(raw_fname)
-    n_secs = 1
-    raw.crop(tmin=0, tmax=n_secs)
     raw_info = raw.info
     sfreq = raw_info['sfreq']
+    n_secs = 1
+    n_requested_samples = math.ceil(sfreq * n_secs)
     with MockLSLStream(host, raw, ch_type='eeg', status=True):
         with LSLClient(info=raw_info, host=host, wait_max=5) as client:
             client_info = client.get_measurement_info()
-            n_samples = math.ceil(sfreq * n_secs * 2)
-            epoch = client.get_data_as_epoch(n_samples=n_samples)
+            epoch = client.get_data_as_epoch(n_samples=n_requested_samples)
+
+            epoch_data = epoch.get_data()
+            n_epochs, n_channels, n_times = epoch_data.shape
+            assert n_epochs == 1
+            assert n_channels == client_info['nchan']
+            assert n_times == n_requested_samples
 
     assert client_info['nchan'] == raw_info['nchan']
     assert ([ch["ch_name"] for ch in client_info["chs"]] ==
             [ch_name for ch_name in raw_info['ch_names']])
 
     assert raw_info['nchan'], sfreq == epoch.get_data().shape[1:]
+
+@requires_pylsl
+def test_connect(mocker):
+    """Mock connect to LSL stream."""
+    # Import pylsl here so that the test can be skipped if pylsl is not installed
+    pylsl = _check_pylsl_installed(strict=True)
+
+    # Constants
+    buffer_size = 17
+    n_channels = 6
+    c_channel_format = pylsl.cf_float32
+    numpy_channel_format = np.float32
+
+    # Replace pylsl streams with a mock
+    mock_resolve_streams = mocker.patch(
+        'pylsl.resolve_streams',
+        return_value=[pylsl.StreamInfo(
+            source_id=host,
+            channel_count=n_channels,
+            channel_format=c_channel_format,
+        )],
+    )
+
+    lsl_client = LSLClient(host=host, buffer_size=buffer_size)
+    # Mock out the pylsl.resolve_streams
+    lsl_client._connect()
+
+    assert isinstance(lsl_client.client, pylsl.StreamInlet)
+    assert lsl_client.buffer.shape == (buffer_size, n_channels)
+    assert lsl_client.buffer.dtype == numpy_channel_format
