727e7b90141bf0a51f4204ddbe55f0ce2f41ce67
Eric Larson <larson.eric.d@gmail.com>
2024-02-26 13:27:50 -0500

    FIX: unit

--- doc/conf.py
+++ doc/conf.py
@@ -371,3 +371,34 @@ numpydoc_xref_ignore = {
     # unlinkable
     'mne_qt_browser.figure.MNEQtBrowser',
 }
+
+# Adapted from MNE-Python
+
+def unit_role(name, rawtext, text, lineno, inliner, options={}, content=[]):  # noqa: B006
+    parts = text.split()
+
+    def pass_error_to_sphinx(rawtext, text, lineno, inliner):
+        msg = inliner.reporter.error(
+            "The :unit: role requires a space-separated number and unit; "
+            f"got {text}",
+            line=lineno,
+        )
+        prb = inliner.problematic(rawtext, rawtext, msg)
+        return [prb], [msg]
+
+    # ensure only two parts
+    if len(parts) != 2:
+        return pass_error_to_sphinx(rawtext, text, lineno, inliner)
+    # ensure first part is a number
+    try:
+        _ = float(parts[0])
+    except ValueError:
+        return pass_error_to_sphinx(rawtext, text, lineno, inliner)
+    # input is well-formatted: proceed
+    node = nodes.Text("\u202F".join(parts))
+    return [node], []
+
+
+def setup(app):
+    app.add_role("unit", unit_role)
+    return
